93228c1b8250b0b000baf41e43e28b6ad65988e1
pranav <pranav.kumar@edufied.edu.vn>
2023-07-23 22:36:28 +0530

    made major color scheme changes

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -2,6 +2,6 @@
 //   ? process.env.BASE_URL
 //   : "http://localhost:3000";
 
-const baseUrl = "https://courseshappynotes.vercel.app";
+const baseUrl = "http://localhost:3000";
 
 export default baseUrl;
